5aae5f5d5a3a1a0e383555394bafbf7876fe3fc5
tamemotoyusuke <tamemotoyou@gmail.com>
2020-08-11 17:07:54 +0900

    update

--- pages/_template/_type/step4/index.vue
+++ pages/_template/_type/step4/index.vue
@@ -1,7 +1,16 @@
 <template>
   <PagesBody :title="'ダウンロード'">
     <template v-slot:body>
-      ダウンロードボタン
+      <div class="text-center px-6 py-4">
+        <button
+          class="mx-auto w-1/2 bg-red-500 hover:bg-red-400 text-white font-semibold py-2 px-4 border rounded"
+        >
+          ダウンロード
+        </button>
+      </div>
+      <div class="text-center mb-20">
+        <ButtonDanger class="mx-6" @linkToPrev="linkToPrev" />
+      </div>
     </template>
   </PagesBody>
 </template>
@@ -12,6 +21,11 @@ export default {
     ...formMapper.mapGetters(["result"]),
     template() {
       return this.$route.params.template
+    },
+  },
+  methods: {
+    linkToPrev() {
+      this.$router.go(-1)
     },
   },
 }
