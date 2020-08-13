5ab60cea553d772fe49d4b8db024e5b323df6ac2
kazuki-komori <komokazu.nankai@gmail.com>
2020-08-13 20:00:26 +0900

    update

--- components/form/FormSocialmedia.vue
+++ components/form/FormSocialmedia.vue
@@ -1,49 +1,70 @@
 <template>
   <FormBody>
     <template v-slot:body>
-      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <div class="grid grid-cols-12 w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label
-          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          class="flex col-span-7 block tracking-wide text-gray-700 text-s font-bold mb-2"
           for="sns1"
         >
-          instagram
+          <img
+            class="w-6 h-6 bg-gray-500 mr-2 mt-3 rounded"
+            src="@/assets/images/button/temp_twitter.svg"
+            alt="twitter アイコン"
+          />
+          <span class="my-auto">
+            https://www.twitter.com/
+          </span>
         </label>
         <input
           id="sns1"
           v-model="$v.form.instagram.$model"
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          type="text"
-          placeholder="your name"
-          @input="input"
-        />
-      </div>
-      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <label
-          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="sns2"
-        >
-          twitter
-        </label>
-        <input
-          id="sns2"
-          v-model="$v.form.twitter.$model"
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          class="col-span-5 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           type="text"
           placeholder="@your account"
           @input="input"
         />
       </div>
-      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <div class="grid grid-cols-12 w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label
-          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          class="col-span-7 flex tracking-wide text-gray-700 text-s font-bold mb-2"
+          for="sns2"
+        >
+          <img
+            class="w-6 h-6 bg-gray-500 mr-2 mt-3 rounded"
+            src="@/assets/images/button/temp_facebook.svg"
+            alt="twitter アイコン"
+          />
+          <span class="my-auto">
+            https://www.facebook.com/
+          </span>
+        </label>
+        <input
+          id="sns2"
+          v-model="$v.form.twitter.$model"
+          class="col-span-5 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          type="text"
+          placeholder="your name"
+          @input="input"
+        />
+      </div>
+      <div class="grid grid-cols-12 w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <label
+          class="col-span-7 flex tracking-wide text-gray-700 text-s font-bold mb-2"
           for="sns3"
         >
-          facebook
+          <img
+            class="w-6 h-6 bg-gray-500 mr-2 mt-3 rounded"
+            src="@/assets/images/button/temp_facebook.svg"
+            alt="instagram アイコン"
+          />
+          <span class="my-auto">
+            https://www.facebook.com/
+          </span>
         </label>
         <input
           id="sns3"
           v-model="$v.form.facebook.$model"
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          class="col-span-5 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           type="text"
           placeholder=""
           @input="input"
