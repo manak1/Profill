86886eab57355438aa979bec4656780c3b9178e9
manak1 <mana23happy@hotmail.co.jp>
2020-08-16 01:03:39 +0900

    Code design type1

--- components/templates/template4/designs/DesignType1.vue
+++ components/templates/template4/designs/DesignType1.vue
@@ -1,8 +1,12 @@
 <template>
   <!-- 名刺デザイン4  -->
   <div class="absolute c-design w-full h-full">
-    <span class="square block"></span>
-    <span class="square2 block"> </span>
+    <span class="square-lg lg:block"></span>
+    <span class="square2-lg lg:block"> </span>
+    <span class="square-sm hidden sm:block lg:hidden"></span>
+    <span class="square2-sm hidden sm:block lg:hidden"> </span>
+    <span class="square block sm:hidden lg:hidden"></span>
+    <span class="square2 block sm:hidden lg:hidden"> </span>
   </div>
 </template>
 
@@ -22,16 +26,49 @@ export default {
 .c-design {
   top: 0;
 }
+
 .square {
   position: relative;
-  top: 16%;
+  top: 25%;
+  width: 60%;
+  height: 24%;
+  background-color: #9ae6b4;
+}
+
+.square2 {
+  position: relative;
+  top: 26%;
+  width: 80%;
+  height: 14%;
+  background-color: #9ae6b4;
+}
+
+.square-lg {
+  position: relative;
+  top: 15%;
   width: 80%;
   height: 30%;
   background-color: #9ae6b4;
 }
-.square2 {
+.square2-lg {
   position: relative;
   top: 20%;
+  width: 60%;
+  height: 10%;
+  background-color: #9ae6b4;
+}
+
+.square-sm {
+  position: relative;
+  top: 20%;
+  width: 80%;
+  height: 30%;
+  background-color: #9ae6b4;
+}
+
+.square2-sm {
+  position: relative;
+  top: 22%;
   width: 60%;
   height: 10%;
   background-color: #9ae6b4;
