e00a0248c0a8a71337ebf59cada9987ac2b42d74
manak1 <mana23happy@hotmail.co.jp>
2020-08-15 21:36:20 +0900

    Code design type5

--- components/templates/template4/designs/DesignType5.vue
+++ components/templates/template4/designs/DesignType5.vue
@@ -1,8 +1,7 @@
 <template>
-  <!-- 名刺デザイン18 -->
+  <!-- 名刺デザイン16 -->
   <div class="absolute c-design w-full h-full">
-    <span class="square block"></span>
-    <span class="square2 block"> </span>
+    <div class="c-type4 w-full h-full"></div>
   </div>
 </template>
 
@@ -21,18 +20,10 @@ export default {
 .c-design {
   top: 0;
 }
-.square {
-  position: relative;
-  top: 100px;
-  width: 80%;
-  height: 30%;
-  background-color: #9ae6b4;
-}
-.square2 {
-  position: relative;
-  top: 140px;
-  width: 60%;
-  height: 10%;
-  background-color: #9ae6b4;
+.c-type4 {
+  background-image: url("~assets/images/templates/template4/bgObject5.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position-x: 100%;
 }
 </style>
