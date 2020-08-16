3cf50ed3186194ba69ea761813cd1da2fc234491
tamemotoyusuke <tamemotoyou@gmail.com>
2020-08-16 10:30:49 +0900

    update

--- pages/_template/_type/step4/index.vue
+++ pages/_template/_type/step4/index.vue
@@ -8,16 +8,14 @@
       </div>
       <div class="text-center mt-24">
         <a
-          class="mx-auto inline-block w-1/2 bg-red-500 hover:bg-red-400 text-white font-semibold py-2 px-4 border rounded"
+          class="inline-flex items-center justify-center w-1/2 bg-red-500 hover:bg-red-400 text-white font-semibold py-2 px-4 border rounded"
           :href="output"
           download="output"
         >
-          ダウンロード
+          <i class="c-icon__download inline-block" />
+          <span class="pl-1">ダウンロード</span>
         </a>
       </div>
-      <!--       <div class="text-center mb-20">
-        <ButtonDanger class="mx-6" @linkToPrev="linkToPrev" />
-      </div> -->
       <div class="flex space-x-3 justify-center mt-20">
         <button-twitter />
         <button-facebook />
@@ -72,4 +70,13 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.c-icon__download {
+  background-size: contain;
+  background-repeat: no-repeat;
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  background-image: url("~assets/images/other/download-icon.svg");
+}
+</style>
