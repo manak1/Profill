65f86079e90506db6abcd17824453ee12ce02375
tamemotoyusuke <tamemotoyou@gmail.com>
2020-10-26 00:15:52 +0900

    update

--- pages/-HeroSection.vue
+++ pages/-HeroSection.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="p-hero bg-indigo-100 mb-12">
     <section class="container py-8 px-4 mx-auto">
-      <div class="lg:flex lg:justify-center mt-8 lg:mt-0 lg:py-32">
+      <div
+        class="lg:flex justify-items-stretch lg:justify-center mt-8 lg:mt-0 lg:py-32"
+      >
         <div class="p-hero__textBox text-center mb-16 lg:text-left lg:mb-0">
           <h2 class="p-hero__text mb-4 text-2xl lg:text-3xl text-gray-700">
             バーチャル名刺を作成しよう!
@@ -16,12 +18,13 @@
             <button-facebook />
           </div>
         </div>
-        <p class="flex justify-center lg:justify-start lg:pl-4">
+        <p class="p-hero__image flex justify-center lg:justify-start lg:pl-4">
           <img
             src="~assets/images/hero/hero.svg"
             width="420"
-            height="336"
+            height="246"
             alt="Profill"
+            class="p-hero__image"
           />
         </p>
       </div>
@@ -42,6 +45,10 @@ export default {}
 @media screen and (min-width: 1024px) {
   .p-hero__textBox {
     width: calc(100% - 500px);
+  }
+  .p-hero__image {
+    width: 420px;
+    height: 246px;
   }
 }
 </style>
