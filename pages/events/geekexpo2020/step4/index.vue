d9e60b9e241a6313265d6aa581865c8700ebed53
manak1 <mana23happy@hotmail.co.jp>
2020-12-02 14:11:28 +0900

    set cursor pointer for download button

--- pages/events/geekexpo2020/step4/index.vue
+++ pages/events/geekexpo2020/step4/index.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="text-center mt-16">
         <a
-          class="inline-flex items-center justify-center w-1/2 bg-red-500 hover:bg-red-400 text-white font-semibold py-2 px-4 border rounded"
+          class="inline-flex items-center justify-center cursor-pointer w-1/2 bg-red-500 hover:bg-red-400 text-white font-semibold py-2 px-4 border rounded"
           @click.prevent="print"
         >
           <i class="c-icon__download inline-block" />
