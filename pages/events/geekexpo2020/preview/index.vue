cde5757b0edd15999306e55fc475708475cecdbf
manak1 <mana23happy@hotmail.co.jp>
2020-12-05 16:50:56 +0900

    code geekexpo2020 preview page

--- pages/events/geekexpo2020/preview/index.vue
+++ pages/events/geekexpo2020/preview/index.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    hogehoge
-  </div>
+  <section class="container py-8 px-4 mx-auto">
+    <EventCardTemplate />
+    <div
+      class="lg:flex lg:justify-center lg:-space-x-8 items-center text-center"
+    >
+      <div class="mt-8 lg:mt-12 mx-auto w-full lg:w-1/3">
+        <p class="font-bold">
+          文字色
+        </p>
+        <object-color-picker type="swatches" class="mt-4" />
+      </div>
+    </div>
+
+    <div class="mt-12 lg:mt-16">
+      <div class="mt-10 text-left">
+        <a
+          href="#"
+          class="hover:text-blue-700 text-blue-500 font-bold py-2 px-4 rounded"
+          @click.prevent="$router.go(-1)"
+        >
+          戻る
+        </a>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import ObjectColorPicker from "@/components/object/ObjectColorPicker"
+export default {
+  components: {
+    ObjectColorPicker,
+  },
+}
 </script>
 
 <style scoped></style>
